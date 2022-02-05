90a3c77a5ffb8a3e158d3f2015d2c7eed5666ae4
Tai Phan Van <phanvantai.sh@gmail.com>
2022-02-05 22:08:38 +0700

    Initial Commit

---
+++ ToDo-SwiftUI-RealmDB/ToDo_SwiftUI_RealmDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_SwiftUI_RealmDBApp.swift
+//  ToDo-SwiftUI-RealmDB
+//
+//  Created by Tai Phan Van on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_SwiftUI_RealmDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
